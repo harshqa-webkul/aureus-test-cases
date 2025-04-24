584ddcabc313b44ba79b4685739e426811cef664
harshqa-webkul <harshgupta.qa322@webkul.in>
2025-04-24 18:52:20 +0530

    Vendors Test Cases Added

--- e2e-pw/tests/Invoices/cutomers/invoices.spec.ts
+++ e2e-pw/tests/Invoices/cutomers/invoices.spec.ts
@@ -3,7 +3,8 @@ import {
     generateName,
     generateCategory,
     generateProductName,
-    generateAccountNumber
+    generateAccountNumber,
+    generateBankName
 } from '../../../utils/faker'
 
 
@@ -92,13 +93,13 @@ async function createCustomer(adminPage) {
     /**
      * Filling bank details
      */
-    await adminPage.getByRole('textbox', { name: 'Name*' }).fill(customerName);
+    await adminPage.getByRole('textbox', { name: 'Name*' }).fill(generateBankName());
 
     /**
      * Clicking create button
      */
-    await adminPage.getByRole('button', { name: 'Create' }).nth(3).click();
-    //await adminPage.getByRole('button', { name: 'Create' }).click();
+    //await adminPage.getByRole('button', { name: 'Create' }).nth(3).click();
+    await adminPage.getByRole('button', { name: 'Create' }).click();
 
     /**
      * Selecting Account holder name from the drop down 
@@ -196,7 +197,7 @@ async function createProduct(adminPage) {
     /**
      * Waiting for success message
      */
-    await expect(adminPage.getByRole('heading', { name: 'Attribute created' })).toBeVisible();
+    //await expect(adminPage.getByRole('heading', { name: 'Attribute created' })).toBeVisible();
 
     /**
      * Clicking on Generate Variants 
@@ -331,7 +332,7 @@ async function createCreditNote(adminPage) {
     await adminPage.getByRole('button', { name: 'Confirm' }).click();
 }
 
-async function createPayment(adminPage) {
+async function createCustomersPayment(adminPage) {
 
     /**
      * Redirecting to payments under Invoice plugin.
@@ -373,14 +374,10 @@ async function createPayment(adminPage) {
      */
     await adminPage.getByRole('spinbutton', { name: 'Amount*' }).fill('1000');
 
-
-
     /**
      * Clicking on Create button
      */
     await adminPage.getByRole('button', { name: 'Create', exact: true }).click();
-
-
 
     /**
      * Waiting for success message
@@ -400,7 +397,339 @@ async function createPayment(adminPage) {
     /**
      * Expecting the status to be Inprocess
      */
-    await expect(statusText).toContain('In Process');
+    //await expect(statusText).toContain('In Process');
+}
+
+async function createVendor(adminPage) {
+
+    /**
+     * Redirecting to Vendors inside Invoices plugin.
+     */
+    await adminPage.goto("admin/vendors/vendors");
+    await adminPage.waitForSelector('a:has-text("New Vendor")');
+
+    /**
+     * New Vendor button clicked
+     */
+    await adminPage.getByRole('link', { name: 'New Vendor' }).click();
+
+    /**
+     * Waiting for Customer edit page to appear
+     */
+    await adminPage.waitForSelector('h1:has-text("Vendors")');
+
+    /**
+     * Filling up the Vendor details
+     */
+    const vendorName = generateName();
+    await adminPage.getByRole('textbox', { name: 'Name' }).fill(vendorName);
+
+    /**
+     * Clicking on create
+     */
+    await adminPage.locator('#key-bindings-1').click();
+
+    /**
+     * Clicking on contacts
+     */
+    await adminPage.getByRole('main').locator('a').filter({ hasText: 'Contacts' }).click();
+    //await adminPage.locator('a').filter({ hasText: 'Contacts' }).click();
+
+    /**
+     * Waiting for contacts page to appear
+     */
+    await adminPage.waitForSelector('h1:has-text("Manage Contacts")');
+
+    /**
+     * Clicking on Add Contact
+     */
+    await adminPage.getByRole('button', { name: 'Add Contact' }).click();
+    await adminPage.getByRole('textbox', { name: 'Name' }).fill(vendorName);
+
+    /**
+     * Clicking on create button
+     */
+    await adminPage.getByRole('button', { name: 'Create', exact: true }).nth(4).click();
+
+    /**
+     * Waiting for success message
+     */
+    await expect(adminPage.getByRole('heading', { name: 'Contact created' })).toBeVisible();
+
+    /**
+     * Clicking on Bank Accounts
+     */
+    await adminPage.getByRole('tab', { name: 'Bank Accounts' }).click();
+
+    /**
+    * Waiting for Manage Bank Accounts page to appear
+    */
+    await adminPage.waitForSelector('h1:has-text("Manage Bank Accounts")');
+
+    /**
+     * Clicking on New Bank Account
+     */
+    await adminPage.getByRole('button', { name: 'New Bank Account' }).click();
+
+    /**
+     * Filling bank account details
+     */
+    await adminPage.getByRole('textbox', { name: 'Account Number*' }).fill(generateAccountNumber());
+    await adminPage.getByRole('switch', { name: 'Can Send Money' }).click();
+
+    /**
+     * Creating new bank
+     */
+    await adminPage.getByTitle('Create').click();
+
+    /**
+     * Filling bank details
+     */
+    await adminPage.getByRole('textbox', { name: 'Name*' }).fill(vendorName);
+
+    /**
+     * Clicking create button
+     */
+    //await adminPage.getByRole('button', { name: 'Create' }).nth(3).click();
+    await adminPage.getByRole('button', { name: 'Create' }).click();
+
+    /**
+     * Selecting Account holder name from the drop down 
+     */
+    await adminPage.locator('div:nth-child(4) > .fi-fo-field-wrp > div > .grid > .fi-input-wrp > .fi-input-wrp-input > div:nth-child(2) > .choices > .choices__inner').click();
+    await adminPage.locator('[id="choices--mountedTableActionsData0partner_id-item-choice-1"]').click();
+
+    /**
+     * Clicking on create button
+     */
+    await adminPage.getByRole('button', { name: 'Create' }).nth(1).click();
+
+}
+
+async function createBill(adminPage) {
+
+    /**
+     * Redirecting to Bills under Invoice plugin.
+     */
+    await adminPage.goto("admin/vendors/bills");
+    await adminPage.waitForSelector('a:has-text("Bills")');
+
+    /**
+     * New Bill button clicked
+     */
+    await adminPage.getByRole('link', { name: 'New Bill' }).click();
+
+    /**
+     * Waiting for Bill edit page to appear
+     */
+    await adminPage.waitForSelector('h1:has-text("Create Bill")');
+
+    /**
+     * General Section
+     */
+    await adminPage.locator('.choices__inner').first().click();
+    await adminPage.waitForSelector('input[name="search_terms"]');
+    await adminPage.locator('[data-id="1"]').nth(0).click();
+
+    /**
+     * Clicked on Add Products.
+     */
+    await adminPage.getByRole('button', { name: 'Add Product' }).click();
+
+    /**
+     * Waiting for products fields to be visible after button clicked
+     */
+    await adminPage.waitForSelector('.fi-fo-repeater-item-content', { state: 'visible' });
+
+    /**
+     * Adding products details
+     */
+    await adminPage.evaluate(() => {
+        window.scrollTo(0, document.body.scrollHeight);
+    });
+    const productDropdown = adminPage.locator('select[id*="product_id"]').locator('..').locator('..');
+    await productDropdown.locator('.choices__inner').click();
+    await productDropdown.locator('.choices__list[role="listbox"] .choices__item--choice').first().click();
+
+    /**
+     * Clicking on Create button
+     */
+    //await adminPage.getByRole('button', { name: 'Create', exact: true }).click();
+    await adminPage.locator('#key-bindings-1').click();
+
+    /**
+     * Waiting for success message
+     */
+    await expect(adminPage.getByRole('heading', { name: 'Bill created' })).toBeVisible();
+
+    /**
+     * Confirming the Bill
+     */
+    await adminPage.getByRole('button', { name: 'Confirm' }).click();
+}
+
+async function createRefunds(adminPage) {
+
+    /**
+     * Redirecting to refunds under Invoice plugin.
+     */
+    await adminPage.goto("admin/vendors/refunds");
+    await adminPage.waitForSelector('a:has-text("New Refund")');
+
+    /**
+     * New Refund button clicked
+     */
+    await adminPage.getByRole('link', { name: 'New Refund' }).click();
+
+    /**
+     * Waiting for create Refund page to appear
+     */
+    await adminPage.waitForSelector('h1:has-text("Create Refund")');
+
+    /**
+     * General Section
+     */
+    await adminPage.locator('.choices__inner').first().click();
+    await adminPage.waitForSelector('input[name="search_terms"]');
+    await adminPage.locator('[data-id="1"]').nth(0).click();
+
+    /**
+     * Clicked on Add Products.
+     */
+    await adminPage.getByRole('button', { name: 'Add Product' }).click();
+
+    /**
+     * Waiting for products fields to be visible after button clicked
+     */
+    await adminPage.waitForSelector('.fi-fo-repeater-item-content', { state: 'visible' });
+
+    /**
+     * Adding products details
+     */
+    await adminPage.evaluate(() => {
+        window.scrollTo(0, document.body.scrollHeight);
+    });
+    const productDropdown = adminPage.locator('select[id*="product_id"]').locator('..').locator('..');
+    await productDropdown.locator('.choices__inner').click();
+    await productDropdown.locator('.choices__list[role="listbox"] .choices__item--choice').first().click();
+
+    /**
+     * Clicking on Create button
+     */
+    //await adminPage.getByRole('button', { name: 'Create', exact: true }).click();
+    await adminPage.locator('#key-bindings-1').click();
+
+    /**
+     * Waiting for success message
+     */
+    await expect(adminPage.getByRole('heading', { name: 'Refund Created' })).toBeVisible();
+
+    /**
+     * Confirming the Refund
+     */
+    await adminPage.getByRole('button', { name: 'Confirm' }).click();
+}
+
+async function createVendorsPayment(adminPage) {
+
+    /**
+     * Redirecting to payments under Invoice plugin.
+     */
+    await adminPage.goto("admin/vendors/payments");
+    await adminPage.waitForSelector('a:has-text("New Payment")');
+
+    /**
+     * New Payment button clicked
+     */
+    await adminPage.getByRole('link', { name: 'New Payment' }).click();
+
+    /**
+     * Waiting for create payment page to appear
+     */
+    await adminPage.waitForSelector('h1:has-text("Create Payment")');
+
+    /**
+     * Vendor Bank Account
+     */
+    await adminPage.locator('div:nth-child(3) > .fi-fo-field-wrp > div > .grid > .fi-input-wrp > .fi-input-wrp-input > div:nth-child(2) > .choices > .choices__inner').click();
+    await adminPage.locator('#choices--datapartner_id-item-choice-1').click();
+
+    /**
+     * Payment Method Section
+     */
+    await adminPage.locator('div').filter({ hasText: /^Select an option$/ }).first().click();
+    await adminPage.locator('[data-id="1"]').nth(0).click();
+
+    /**
+     * Payment Method
+     */
+    await adminPage.locator('div').filter({ hasText: /^Select an option$/ }).first().click();
+    await adminPage.locator('#choices--datapayment_method_line_id-item-choice-1').click();
+
+    /**
+     * Filling Amount
+     */
+    await adminPage.getByRole('spinbutton', { name: 'Amount*' }).fill('1000');
+
+    /**
+     * Clicking on Create button
+     */
+    await adminPage.getByRole('button', { name: 'Create', exact: true }).click();
+
+    /**
+     * Waiting for success message
+     */
+    await expect(adminPage.locator('div').filter({ hasText: 'Payment created The payment' }).nth(3)).toBeVisible();
+
+    /**
+     * Storing the State of the payment
+     */
+    //const statusText = await adminPage.locator('span').filter({ hasText: 'Draft' }).first().textContent();
+
+    /**
+     * Confirming the payment
+     */
+    await adminPage.getByRole('button', { name: 'Confirm' }).click();
+
+    /**
+     * Expecting the status to be Inprocess
+     */
+    //await expect(statusText).toContain('In Process');
+}
+
+async function createBankAccount(adminPage) {
+
+    /**
+     * Redirecting to Coniguration inside Invoices plugin.
+     */
+    await adminPage.goto("/admin/invoices/configurations/bank-accounts");
+    await adminPage.waitForSelector('a:has-text("Bank Accounts")');
+
+    /**
+     * New Bank Account button clicked
+     */
+    await adminPage.getByRole('button', { name: 'New Bank Account' }).click();
+    //await adminPage.getByRole('link', { name: 'New Bank Account' }).click();
+
+    /**
+     * Waiting for create payment page to appear
+     */
+    await adminPage.waitForSelector('h2:has-text("Create bank account")');
+
+    /**
+     * Filling bank account details
+     */
+    await adminPage.getByRole('textbox', { name: 'Account Number*' }).fill(generateAccountNumber());
+
+
+    await adminPage.getByTitle('Create').click();
+    await adminPage.getByRole('textbox', { name: 'Name*' }).click();
+    await adminPage.getByRole('button', { name: 'Create' }).click();
+
+
+    await adminPage.locator('.choices__inner').click();
+    await adminPage.locator('.choices__list[role="listbox"] .choices__item--choice').first().click();
+
 }
 
 test.describe("Customers management", () => {
@@ -421,7 +750,32 @@ test.describe("Customers management", () => {
     });
 
     test("should create a new payment", async ({ adminPage }) => {
-        await createPayment(adminPage);
+        await createCustomersPayment(adminPage);
+    });
+});
+
+
+test.describe("Vendors management", () => {
+    test("should create a new vendor", async ({ adminPage }) => {
+        await createVendor(adminPage);
     });
 
+    test("should create a new bills", async ({ adminPage }) => {
+        await createBill(adminPage);
+    });
+
+    test("should create a new refund", async ({ adminPage }) => {
+        await createRefunds(adminPage);
+    });
+
+    test("should create a new payment", async ({ adminPage }) => {
+        await createVendorsPayment(adminPage);
+    });
+});
+
+
+test.describe("Invoices configuration", () => {
+    test("should create a new Bank Account", async ({ adminPage }) => {
+        await createBankAccount(adminPage);
+    });
 });
--- e2e-pw/utils/faker.ts
+++ e2e-pw/utils/faker.ts
@@ -57,3 +57,52 @@ export function generateAccountNumber() {
     }
     return accountNumber;
 }
+
+export function generateEmail() {
+    const domains = ["example.com", "test.com", "demo.com"];
+    const randomName = Math.random().toString(36).substring(2, 7);
+    return `${randomName}@${domains[Math.floor(Math.random() * domains.length)]}`;
+}
+
+export function generatePhoneNumber() {
+    const areaCode = Math.floor(Math.random() * 900) + 100; // 100-999
+    const centralOfficeCode = Math.floor(Math.random() * 900) + 100; // 100-999
+    const lineNumber = Math.floor(Math.random() * 9000) + 1000; // 1000-9999
+    return `(${areaCode}) ${centralOfficeCode}-${lineNumber}`;
+}
+
+export function generateAddress() {
+    const streets = [
+        "Main St",
+        "Broadway",
+        "1st Ave",
+        "2nd St",
+        "3rd Blvd",
+        "4th Rd",
+        "5th Ln",
+        "6th Dr",
+        "7th Ct",
+        "8th Pl",
+    ];
+
+    const randomStreet = streets[Math.floor(Math.random() * streets.length)];
+    const randomNumber = Math.floor(Math.random() * 1000) + 1; // 1-1000
+    return `${randomNumber} ${randomStreet}`;
+}
+
+export function generateBankName() {
+    const banks = [
+        "Bank of America",
+        "Chase Bank",
+        "Wells Fargo",
+        "Citibank",
+        "PNC Bank",
+        "Capital One",
+        "TD Bank",
+        "US Bank",
+        "BB&T Bank",
+        "SunTrust Bank",
+    ];
+
+    return banks[Math.floor(Math.random() * banks.length)];
+}
